88e96e6602c0324d13a91c5b6a640445f1fad6e1
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-11 19:29:19 +0200

    Created constructor with user name and changeStateData to dynamically change current user

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: "Linux Kyrios"
+        }
+    }
+
+    //For dynamically change data of current user
+    changeStateData = () => {
+        this.setState({
+            userName: this.state.userName === "Linux Kyrios" ? "Linux Atrocius" : "Linux Kyrios"
+        })
+    }
+
     render() {
         return (
             <div>
                 <h4 className="bg-primary text-white text-center p2">
-                    Tasks List
+                    Tasks List of user { this.state.userName }
                 </h4>
+                <button className="btn btn-primary m-2"
+                        onClick={this.changeStateData}>
+                    Change user
+                </button>
             </div>
         )
     };
