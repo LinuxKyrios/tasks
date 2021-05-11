8327ec4e81322c596d53cfa7cb3e0e20004c94d3
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-11 20:20:59 +0200

    Created new methods for adding new tasks to task array, and created starting content od task array

--- src/App.js
+++ src/App.js
@@ -7,26 +7,49 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userName: "Linux Kyrios"
+            userName: "Linux Kyrios",
+            todoStuff: [{action: "Check out the arrival of Macs with the new M2 processors", done: false},
+                        {action: "Start looking for a new car", done: false},
+                        {action: "Buy the biggest delicous Buenos Nachos Pizza", done: true},
+                        {action: "Remove ex number from my telephone", done: false}],
+            newItemText: ""
         }
     }
 
-    //For dynamically change data of current user
-    changeStateData = () => {
-        this.setState({
-            userName: this.state.userName === "Linux Kyrios" ? "Linux Atrocius" : "Linux Kyrios"
-        })
+    //Method for update new text value
+    updateNewTextvalue = (event) => {
+        this.setState({ newItemText: event.target.value });
     }
 
-    //Render function returns upper tab with title and button to change user
+    //Method for creating new to do task and add it to array
+    createNewTodo = () => {
+        if (!this.state.todoStuff
+            .find(item => item.action === this.state.newItemText)) {
+                this.setState({
+                todoStuff: [...this.state.todoStuff,
+                    { action: this.state.newItemText, done: false}],
+                newItemText: ""
+            });
+        }
+    }
+
+    //Render function returns upper tab with title and button to add new task
     render = () =>
             <div>
-                <h4 className="bg-primary text-white text-center p2">
+                <h4 className="bg-primary text-white text-center p-2">
                     Tasks List of user { this.state.userName }
+                    (Task List: {this.state.todoStuff.filter(t => !t.done).length})
                 </h4>
-                <button className="btn btn-primary m-2"
-                        onClick={this.changeStateData}>
-                    Change user
-                </button>
+                <div className="container-fluid">
+                    <div className="my-1">
+                        <input classname="form-control"
+                            value={this.state.newItemText}
+                            onChange={this.updateNewTextValue} />
+                            <p>
+                                <button className="btn btn-primary mt-1"
+                                onClick={this.createNewTodo}>Add new</button>
+                            </p>
+                    </div>
+                </div>
             </div>
     };
