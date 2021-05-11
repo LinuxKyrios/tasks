bf834a85cae423b206e7a16a1c298349850e97b4
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-11 21:41:29 +0200

    Created VisibleController.js file with content

---
+++ src/VisibilityController.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//Component created for control visibility of accomplished tasks
+
+export class VisibilityController extends Component {
+
+    render = () =>
+        <div className="form-check">
+            <input className="form-check-input" type="checkbox"
+                checked={this.props.isChecked}
+                onChange={(v) => this.props.callback(v.target.checked)} />
+            <label className="form-check-label">
+                Show completed tasts {this.props.description}
+            </label>
+        </div>
+}
